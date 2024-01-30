318dc95392a24a5da642f4101a6e9ef420bad895
Ivan Surovegin <givenrous@yandex.ru>
2024-01-30 11:46:07 +0300

    решение

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,14 @@
+import read from "./reader.js";
+import json from "./parser.js";
+import GameSaving from "./GameSaving.js";
+
+export default class GameSavingLoader {
+  static load() {
+    return read()
+      .then((data) => json(data))
+      .then((value) => JSON.parse(value))
+      .then(
+        ({ id, created, userInfo }) => new GameSaving(id, created, userInfo)
+      );
+  }
+}
---
+++ src/js/__tests__/basic.test.js
@@ -0,0 +1,13 @@
+import GameSavingLoader from "../GameSavingLoader.js";
+
+describe("GameSavingLoader class", () => {
+  it("should return correct data", async () => {
+    const result = await GameSavingLoader.load();
+    const expected = {
+      id: 9,
+      created: 1546300800,
+      userInfo: { id: 1, name: "Hitman", level: 10, points: 2000 },
+    };
+    expect(result).toEqual(expected);
+  });
+});
